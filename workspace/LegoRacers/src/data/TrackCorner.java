be5990f356570888b305f5ac661decd4919d9d73
JaynsVerdammt <jm-jensmueller@web.de>
2016-04-28 11:24:19 +0200

    Nullobjekte erzeugt - Visio Dokumente
    Klassendiagramm + Aktivitätsdiagramm hinzugefügt
    Genutzte Funktionen als Nullobjekt erzeugt

--- workspace/LegoRacers/src/data/TrackCorner.java
+++ workspace/LegoRacers/src/data/TrackCorner.java
@@ -2,4 +2,14 @@ package data;
 
 public class TrackCorner extends TrackData{
 
+	public int getDirection() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getSteeringRate() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
