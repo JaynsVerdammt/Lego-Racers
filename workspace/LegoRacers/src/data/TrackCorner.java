5b98a50ff07fe07bc5387d7ed6eb47a8c9dfc694
knockout04 <jan.herkommer@googlemail.com>
2016-05-12 11:13:15 +0200

    Einführungsrunde implementiert

--- workspace/LegoRacers/src/data/TrackCorner.java
+++ workspace/LegoRacers/src/data/TrackCorner.java
@@ -1,7 +1,10 @@
 package data;
 
 public class TrackCorner extends TrackData{
-
+	
+	public static final int LEFT = 0;
+	public static final int RIGHT = 1;
+	
 	public int getDirection() {
 		// TODO Auto-generated method stub
 		return 0;
@@ -11,5 +14,20 @@ public class TrackCorner extends TrackData{
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	
+	public int getDelay() {
+		return 0;
+	}
+	
+	public void setDirection(int direction) {
+		
+	}
+	
+	public void setSteeringRate(int newRate) {
+		
+	}
+	
+	public void setDelay(int delay) {
+		
+	}
 }
--- workspace/LegoRacers/src/sensors/RGBControl.java
+++ workspace/LegoRacers/src/sensors/RGBControl.java
@@ -3,9 +3,18 @@ package sensors;
 import controller.MainControl;
 
 public class RGBControl extends Thread{
-
+	
+	public static final int BLACK = 0;
+	public static final int WHITE = 1;
+	public static final int RED = 2;
+	public static final int GREEN = 3;
+	
 	public RGBControl(MainControl mainControl) {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public int getColor() {
+		return 0;
 	}
 
 }
