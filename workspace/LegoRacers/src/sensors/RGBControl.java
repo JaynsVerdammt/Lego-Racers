e7596212d1a8ba4fe773ada604466b46c62661ad
leilale <lara.kroesen@gmail.com>
2016-05-24 22:21:43 +0200

    RGB Sensor: Rückgabe der Farbe

--- workspace/LegoRacers/src/sensors/RGBControl.java
+++ workspace/LegoRacers/src/sensors/RGBControl.java
@@ -1,20 +1,26 @@
 package sensors;
 
 import controller.MainControl;
+import lejos.nxt.ColorSensor;
+import lejos.nxt.SensorPort;
+import lejos.robotics.Color;
 
 public class RGBControl extends Thread{
 	
-	public static final int BLACK = 0;
-	public static final int WHITE = 1;
-	public static final int RED = 2;
-	public static final int GREEN = 3;
+	public static final int BLACK = Color.BLACK;
+	public static final int WHITE = Color.WHITE;
+	public static final int RED = Color.RED;
+	public static final int GREEN = Color.GREEN;
 	
 	public RGBControl(MainControl mainControl) {
 		// TODO Auto-generated constructor stub
 	}
 	
 	public int getColor() {
-		return 0;
+		ColorSensor colorSensor = new ColorSensor(SensorPort.S1);
+		colorSensor.getColorID();
+		
+		return colorSensor.getColorID();
 	}
 
 }
